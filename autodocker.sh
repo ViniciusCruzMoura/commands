c9b636ea9be7a6e29a0c63a7ad6875079a0cebb9
ViniciusCruzMoura <contato.cruz.moura@gmail.com>
2024-11-19 08:24:18 -0400

    change(autodocker): improve build and run logic, add checks for existing images

--- autodocker.sh
+++ autodocker.sh
@@ -3,32 +3,37 @@
 #set -xe
 
 case $1 in
-	-b|-build|--build|build)
+    -b|-build|--build|build)
         docker build --network=host -t $(basename $PWD) .
-		exit 0
-		;;
-	-r|-run|--run|run)
+        exit 0
+        ;;
+    -r|-run|--run|run)
+        if docker images --format '{{.Repository}}:{{.Tag}}' | grep -q "^$(basename $PWD):latest$"; then
+            echo
+        else
+            docker build --network=host -t $(basename $PWD) .
+        fi
         docker run -it --rm --network=host --env-file .env --name $(basename $PWD) -v "$PWD":/"$(basename $PWD)" -w /"$(basename $PWD)" $(basename $PWD) bash
-		exit 0
-		;;
-	-rmi|--rmi|rmi)
+        exit 0
+        ;;
+    -rmi|--rmi|rmi)
         docker rmi $(basename $PWD) --force
-		exit 0
-		;;
+        exit 0
+        ;;
     -h|-help|--help|help|*)
-		echo "These are common commands used in various situations:"
+        echo "These are common commands used in various situations:"
         echo 
         echo "Uso:"
         echo "  autodocker COMMAND"
         echo
         echo "Commands:"
-		echo "  help            Shows this information"
-		echo "  build           Make image"
-		echo "  run             Run image"
-		echo "  rmi             Remove image"
+        echo "  help            Display this help information."
+        echo "  build           Build a new Docker image."
+        echo "  run             Run the Docker image if it exists; otherwise, build the image and then run it."
+        echo "  rmi             Remove the Docker image by untagging it."
         echo 
         echo "autodocker COMMAND --help to get help on each command"
         echo		
         exit 0
-		;;
+        ;;
 esac
