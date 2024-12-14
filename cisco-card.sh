1b13d1335bb3c72f2d026113c861dc39860e6c60
ViniciusCruzMoura <contato.cruz.moura@gmail.com>
2024-12-14 00:17:22 -0400

    change(cisco-card): add anyconnect

--- cisco-card.sh
+++ cisco-card.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 set -xe
 #openconnect vpnprod.redetendencia.com.br
-openconnect cg5567.redetendencia.com.br
+#openconnect cg5567.redetendencia.com.br
+case $1 in
+    c)
+        /opt/cisco/anyconnect/bin/vpn connect cg5567.redetendencia.com.br
+        exit 0
+        ;;
+    d)
+        /opt/cisco/anyconnect/bin/vpn disconnect cg5567.redetendencia.com.br
+        exit 0
+        ;;
+esac
