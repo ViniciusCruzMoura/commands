ef985e9a39d780d87a03413b1c84cf89cea06d6e
ViniciusCruzMoura <contato.cruz.moura@gmail.com>
2025-01-22 23:43:18 -0400

    change gitlog: add author name

--- gitlog.sh
+++ gitlog.sh
@@ -2,4 +2,4 @@
 #set -xe
 #git log --pretty=format:'%C(auto)%h%d (%cr) %cn <%ce> %s' --no-merges
 #git log --pretty=format:"%C(yellow)%h%Creset%C(cyan)%C(bold)%d%Creset %C(cyan)(%cr)%Creset %C(green)%ce%Creset %s" --no-merges
-git log --pretty=format:"%C(yellow)%h%x09%Creset%C(cyan)%C(bold)%ad%Creset  %C(green)%Creset %s" --date=short --no-merges
+git log --pretty=format:"%C(yellow)%h%x09%Creset%C(cyan)%C(bold)%ad%Creset  %C(green)%Creset %s | %C(yellow)%C(bold)%an%Creset" --date=short --no-merges
